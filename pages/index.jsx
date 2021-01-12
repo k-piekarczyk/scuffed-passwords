caefc9b5c76276d0c22444a2370bec326b61ed5d
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2021-01-12 23:19:43 +0100

    add basic nextjs app

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+function Home() {
+    return (
+        <>
+            <h1>Home Page</h1>
+            <Link href='/about'>About</Link>
+        </>
+    )
+}
+
+export default Home
