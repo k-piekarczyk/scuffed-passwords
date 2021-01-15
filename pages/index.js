08d627c605461ad074066e1484e3e5e7edc1a0c4
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2021-01-15 16:06:10 +0100

    add some text to the main page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,33 @@
 import Navigation from '../components/navigation'
-import ConditionalRender from '../components/conditionalRender'
+
+import { Jumbotron, Container } from 'react-bootstrap'
+
+import { FaGithub, FaHeart } from 'react-icons/fa'
 
 function Home () {
   return (
     <>
       <Navigation />
-      <ConditionalRender server>
-        <p>Rendered on the server</p>
-      </ConditionalRender>
-      <ConditionalRender client>
-        <p>Rendered on the client</p>
-      </ConditionalRender>
+      <Container className='mt-3'>
+        <Jumbotron>
+          <h1>Welcome to <code>Scuffed Passswords</code></h1>
+          <p>
+            A simple yet somewhat secure password storage.
+          </p>
+        </Jumbotron>
+        <Container>
+          <h4>Disclaimer</h4>
+          <p className='text-justify'>
+            Do NOT actually use it! I am in no way, shape or form an expert on security,
+            (at the time of writing this), and although it is an attempt to display at least some understanding of application
+            security, you shoud treat it for what it is: a student project.
+          </p>
+
+          <p className='text-monospace text-md-center mt-5'>
+            Made with <FaHeart /> by <a href='https://github.com/k-piekarczyk'>k-piekarczyk <FaGithub /></a>
+          </p>
+        </Container>
+      </Container>
     </>
   )
 }
